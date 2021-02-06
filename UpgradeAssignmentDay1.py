96eb1640845d190aaf8ca8b8b2dcb12885bc3ca4
TejaVardhanRaju <78658432+TejaVardhanRaju@users.noreply.github.com>
2021-02-06 23:30:53 +0530

    Add files via upload

---
+++ UpgradeAssignmentDay1.py
@@ -0,0 +1,19 @@
+#Q1.Write a python program that takes two numbers as the input and print the result of Addition,
+#Subtraction, Multiplication, Division of the two numbers
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Addition is:", x+y)
+print("Substraction is:", x-y)
+print("Multiplication is:", x*y)
+print("Divison is:", int(x/y))
+
+'''Q2.Write a python program that takes two numbers as the input such as X and Y and print the result
+of X^Y(X to the power of Y)'''
+
+X = int(input("Enter first number: "))
+Y = int(input("Print second number"))
+
+print(X, "to the power of", Y, "is: ", X**Y)
+
